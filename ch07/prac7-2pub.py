03aa05eeabb4c5f3696051a56bc9afeeba507d4e
AA <180617768+jhshin23@users.noreply.github.com>
2025-10-18 20:32:42 +0900

    ch07, Page349, P02, 수정: loop_start() 추가

--- ch07/prac7-2pub.py
+++ ch07/prac7-2pub.py
@@ -6,6 +6,7 @@ ip = input("브로커의 IP 주소")
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connect(ip, 1883)
+client.loop_start()
 
 while True:
 	room = str(random.randint(0, 3))
@@ -13,3 +14,5 @@ while True:
 	humidity = str(random.randint(0, 100))
 	client.publish("sensor",room+":"+temp+":"+humidity)
 	time.sleep(1)
+client.loop_stop()
+client.disconnect()
